5ffb2265d4ff9e8891d6a2a288e287998ac65176
Edmilson de Azevedo Cassecasse <55765693+eacassecasse@users.noreply.github.com>
2024-10-23 21:41:00 +0000

    fix: Type annotate url on decorator

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """ This module defines a function name get_page. """
 
-from typing import Callable
 import redis
 import requests
 from functools import wraps
+from typing import Callable
 
 
 _redis = redis.Redis()
@@ -13,16 +13,16 @@ _redis = redis.Redis()
 def cache(fn: Callable) -> Callable:
     """ Manages the caching for a request. """
     @wraps(fn)
-    def caching(url) -> str:
+    def caching(url: str) -> str:
         """ Caches the response content from a HTTP request. """
         _redis.incr("count:{}".format(url))
-        output = _redis.get("response:{}".format(url))
+        output = _redis.get("output:{}".format(url))
 
         if output:
             return output.decode('utf-8')
         output = fn(url)
         _redis.set("count:{}".format(url), 0)
-        _redis.setex("response:{}".format(url), 10, output)
+        _redis.setex("output:{}".format(url), 10, output)
         return output
     return caching
 
